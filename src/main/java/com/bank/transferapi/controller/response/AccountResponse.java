8a6155698c189d7956c169168c4baabb029ed74d
Miaritiana <hei.miaritiana@gmail.com>
2023-03-08 20:55:16 +0300

    chore: add rest response

--- src/main/java/com/bank/transferapi/controller/response/AccountResponse.java
+++ src/main/java/com/bank/transferapi/controller/response/AccountResponse.java
@@ -1,2 +1,17 @@
-package com.bank.transferapi.controller.response;public class AccountResponse {
+package com.bank.transferapi.controller.response;
+
+import com.bank.transferapi.model.Customer;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Builder
+@EqualsAndHashCode
+public class AccountResponse {
+    private long idAccount;
+    private Customer customer;
+    private double balance;
 }
---
+++ src/main/java/com/bank/transferapi/controller/response/CustomerResponse.java
@@ -0,0 +1,18 @@
+package com.bank.transferapi.controller.response;
+
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Builder
+@EqualsAndHashCode
+public class CustomerResponse {
+    private long idCustomer;
+    private String firstName;
+    private String name;
+    private Long phoneNumber;
+    private String username;
+}
