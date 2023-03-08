f9343220b0b09ef2b0004db03c2414e8f29b719e
Miaritiana <hei.miaritiana@gmail.com>
2023-03-08 21:27:53 +0300

    chore: add exception handler

--- src/main/java/com/bank/transferapi/exception/ApiException.java
+++ src/main/java/com/bank/transferapi/exception/ApiException.java
@@ -1,4 +1,11 @@
 package com.bank.transferapi.exception;
 
-public class ApiException {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ApiException extends RuntimeException{
+    public ApiException(int code, String message) {
+        super(HttpStatus.valueOf(code) + " : " + message);
+    }
 }
